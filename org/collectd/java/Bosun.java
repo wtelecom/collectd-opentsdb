a3a84225433f166750936817cb9a7c7e9032db1e
Sergio Soto <ssoto@wtelecom.es>
2015-04-21 10:50:06 +0200

    Stable version of Bosun http plugin sender

--- org/collectd/java/Bosun.java
+++ org/collectd/java/Bosun.java
@@ -1,46 +1,46 @@
+package org.collectd.java;
 
-import org.collectd.api.Collectd;
-import org.collectd.api.ValueList;
-import org.collectd.api.DataSet;
-import org.collectd.api.DataSource;
-import org.collectd.api.CollectdConfigInterface;
-import org.collectd.api.CollectdInitInterface;
-import org.collectd.api.CollectdWriteInterface;
-import org.collectd.api.OConfigValue;
-import org.collectd.api.OConfigItem;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.collectd.api.Collectd;
+import org.collectd.api.CollectdConfigInterface;
+import org.collectd.api.CollectdInitInterface;
+import org.collectd.api.CollectdWriteInterface;
+import org.collectd.api.DataSource;
+import org.collectd.api.OConfigItem;
+import org.collectd.api.OConfigValue;
+import org.collectd.api.ValueList;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Bosun implements CollectdWriteInterface,
     CollectdInitInterface,
     CollectdConfigInterface
 {
-    private String    server = "localhost";
-    private String    port   = "8070";
-    private String    plugin_name = "Bosun";
-
+    private String      server = "localhost";
+    private String      port   = "8070";
 
     public Bosun ()
     {
-        Collectd.registerInit   (plugin_name, this);
-        Collectd.registerWrite  (plugin_name, this);
-        Collectd.registerConfig (plugin_name, this);
+        Collectd.registerInit   ("Bosun", this);
+        Collectd.registerWrite  ("Bosun", this);
+        Collectd.registerConfig ("Bosun", this);
     }
 
     public int init ()
     {
-        try {
-          Collectd.logInfo ("Bosun plugin: server: " + server + ", port: " + port);
-          
-        } catch (UnknownHostException e) {
-          Collectd.logError ("Couldn't establish connection: " + e.getMessage());
-          System.exit(1);
-        } 
+        
+      Collectd.logInfo ("Bosun plugin: server: " + server + ", port: " + port);
 
         return(0);
     }
@@ -50,18 +50,26 @@ public class Bosun implements CollectdWriteInterface,
     List<DataSource> ds = vl.getDataSet().getDataSources();
     List<Number> values = vl.getValues();
     int size            = values.size();
+    StringBuffer sb 	= new StringBuffer();
+    Map<String, String> tagMap = new LinkedHashMap<String, String>();
 
     for (int i=0; i<size; i++) {
-        
+    	
+    	tagMap.clear();
+    	
+    	sb.setLength(0);
+        sb.append("put ");
+    	
         // Metric name
-        String    name, 
-                  pointName,
-                  plugin, 
-                  pluginInstance,
-                  type, 
-                  typeInstance;
+        String    name, pointName,
+                  plugin, pluginInstance,
+                  type, typeInstance,
+                  json;
+        
         ArrayList<String> parts = new ArrayList<String>();
         ArrayList<String> tags = new ArrayList<String>();
+        
+        
 
         plugin         = vl.getPlugin();
         pluginInstance = vl.getPluginInstance();
@@ -70,30 +78,32 @@ public class Bosun implements CollectdWriteInterface,
 
         Collectd.logInfo("plugin: " + plugin + " pluginInstance: " + pluginInstance + " type: " + type + " typeInstance: " + typeInstance);
 
-        
-        
         if ( plugin != null && !plugin.isEmpty() ) {
-          parts.add(plugin);
-          if ( pluginInstance != null && !pluginInstance.isEmpty() ) {
-              tags.add(plugin + "_instance=" + pluginInstance);
-          }
-          if ( type != null && !type.isEmpty()) {
-              tags.add(plugin + "_type=" + type);
-          }
-          if ( typeInstance != null && !typeInstance.isEmpty() ) {
-              tags.add(plugin + "_type_instance=" + typeInstance);
-          }
+            parts.add(plugin);
+            if ( pluginInstance != null && !pluginInstance.isEmpty() ) {
+                tags.add(plugin + "_instance=" + pluginInstance);
+                tagMap.put(plugin+"_instance", pluginInstance);
+            }
+            if ( type != null && !type.isEmpty()) {
+                tags.add(plugin + "_type=" + type);
+                tagMap.put(plugin+"_type", type);
+            }
+            if ( typeInstance != null && !typeInstance.isEmpty() ) {
+                tags.add(plugin + "_type_instance=" + typeInstance);
+                tagMap.put(plugin+"_type_instance", typeInstance);
+            }
 
-          pointName = ds.get(i).getName();
-          if (!pointName.equals("value")) {
-            // Collectd.logInfo("pointName: " + pointName);
-            tags.add(plugin + "_point=" + pointName);
-          }
+            pointName = ds.get(i).getName();
+            if (!pointName.equals("value")) {
+              // Collectd.logInfo("pointName: " + pointName);
+              tags.add(plugin + "_point=" + pointName);
+              tagMap.put(plugin + "_point", pointName);
+            }
         }
 
-        
+        name = join(parts, ".");
 
-        sb.append("\"name\":" + join(parts, ".") + "\",");
+        sb.append(name).append(' ');
 
         // Time
         long time = vl.getTime() / 1000;
@@ -111,43 +121,16 @@ public class Bosun implements CollectdWriteInterface,
         sb.append("source=collectd");
 
         sb.append(" ").append(join(tags, " "));
-
-        String output = sb.toString();
-
+        
+        Collectd.logInfo("StringBuffer: " + sb.toString());
+        
+        json = getJson(name, val, time, host, tagMap);
+        Collectd.logInfo("JSON: " + json);
         // Send to Bosun
-        String json =this.sendJSON()
-        Collectd.logInfo(output);
+        httpPost(json, server, port);
     }
 
     return(0);
-  }
-
-  /*
-  *  JSON format:
-  *
-  *    "{\"metric\":\"custom.my.metic\",\"timestamp\":" + unixTime + ",\"value\":14,\"tags\":{\"host\":\"ssotoTest16\"}}"
-  *
-  */
-  private int sendJSON(String json){
-    
-    HttpClient httpClient = HttpClientBuilder.create().build(); 
-    int result = 0;
-    try {
-        HttpPost request = new HttpPost( "http://" + this.server + this.port+ "/api/post");
-        StringEntity params =new StringEntity("details={\"name\":\"myname\",\"age\":\"20\"} ");
-        request.addHeader("content-type", "application/x-www-form-urlencoded");
-        request.setEntity(params);
-        HttpResponse response = httpClient.execute(request);
-
-        // handle response here...
-    }catch (Exception ex) {
-        // handle exception here
-        Collectd.logError("Error sending json request: " + ex.toString());
-        result = 1;
-    } finally {
-        httpClient.getConnectionManager().shutdown();
-        return result;
-    }
   }
 
   public static String join(Collection<String> s, String delimiter) {
@@ -199,4 +182,65 @@ public class Bosun implements CollectdWriteInterface,
 
     return (0);
   } /* }}} int config */
+  
+	/*
+	* Returns JSON well formed ready to send
+	*/
+  	public static String getJson(String metric_name, Number value, long timestamp, String hostname, Map<String, String> tagMap){
+		
+		// compone the JSON
+		JSONObject json = new JSONObject();
+		
+		try {
+			json.put("metric", metric_name);
+			json.put("timestamp", timestamp);
+			json.put("value", value);
+			
+			JSONObject tagsJson = new JSONObject();
+			tagsJson.put("host", hostname);
+			
+			for (Map.Entry<String, String> entry : tagMap.entrySet())
+			{
+			    tagsJson.put(entry.getKey(), entry.getValue());
+			}
+			
+			json.put("tags", tagsJson);
+			
+		} catch (JSONException e) {
+			Collectd.logError("Error creating JSON string." + e.getStackTrace());
+			e.printStackTrace();
+		}
+		return json.toString();			
+	}
+	
+	@SuppressWarnings("deprecation")
+	public static void httpPost(String json, String server, String port){
+		HttpClient httpClient = HttpClientBuilder.create().build(); //Use this instead 
+	    HttpResponse response = null;
+	    try {
+				
+			HttpPost request = new HttpPost( "http://" + server + ":" + port+ "/api/put");
+				
+	        //StringEntity json_string =new StringEntity("{\"metric\":\"custom.my.metic\",\"timestamp\":" + unixTime + ",\"value\":14,\"tags\":{\"host\":\"hostname\"}}");
+	        request.addHeader("content-type", "application/x-www-form-urlencoded");
+	        request.setEntity(new StringEntity(json));
+	        response = httpClient.execute(request);
+	
+	        // handle response here...
+	        int status_code = response.getStatusLine().getStatusCode();
+	        if ( status_code > 207){
+	        	Collectd.logError( "Request sent failed. Status " + status_code);
+	        } 
+//	        else {
+//	        	Collectd.logInfo("Request succes. Status code: " + status_code);
+//	        }
+	    }catch (Exception ex) {
+	        // handle exception here
+	    	Collectd.logError("Error sending json request: " + ex.toString());
+	    } finally {
+	        httpClient.getConnectionManager().shutdown();
+	    }
+	}
+	
+  
 }
